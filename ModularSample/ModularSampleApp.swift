0c5f320b661aa61c8c97fd299b97b526d1b750ab
Gaurang Lathiya <gaurang.lathiya8@gmail.com>
2025-06-01 11:42:27 +0530

    Initial Commit

---
+++ ModularSample/ModularSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModularSampleApp.swift
+//  ModularSample
+//
+//  Created by Gaurang Lathiya on 01/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ModularSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
