de7d5f80f0912d71df79f34d94b3278abed9272d
Gaurang Lathiya <gaurang.lathiya8@gmail.com>
2025-06-12 09:47:08 +0530

    Removal the usage of GeometryReader

--- UserListModule/Sources/UserListModule/UserListScreen.swift
+++ UserListModule/Sources/UserListModule/UserListScreen.swift
@@ -18,46 +18,44 @@ public struct UserListScreen: View {
     
     public var body: some View {
         NavigationStack {
-            GeometryReader { geometry in
-                ScrollView {
-                    getUserListView(withSize: geometry.size)
-                }
-                .background(Color.clear)
-                .task {
-                    try? await viewModel.fetchUsers()
-                }
-                .navigationTitle("Users")
+            VStack {
+                getUserListView()
             }
+            .frame(height: .infinity)
+            .background(Color.clear)
+            .task {
+                try? await viewModel.fetchUsers()
+            }
+            .navigationTitle("Users")
         }
     }
     
     @ViewBuilder
-    func getUserListView(withSize size: CGSize) -> some View {
+    func getUserListView() -> some View {
         switch viewModel.state {
         case .loading:
             ProgressView {
                 Text(AppConstants.loadingText)
             }
-            .frame(width: size.width, height: size.height)
         case .error(let error):
             Text("Error: \(error.localizedDescription)")
                 .foregroundStyle(.red)
                 .padding(.horizontal)
-                .frame(width: size.width, height: size.height)
         case .loaded:
             if viewModel.users.isEmpty {
                 ContentUnavailableView(AppConstants.noUsersFound,
                                        systemImage: "person.2.slash.fill")
-                    .frame(width: size.width, height: size.height)
             } else {
-                LazyVStack(spacing: 12) {
-                    ForEach(viewModel.filteredUsers, id: \.id) { user in
-                        NavigationLink(destination: UserDetailScreen(user: user)) {
-                            UserListRowView(user: user)
+                ScrollView {
+                    LazyVStack(spacing: 12) {
+                        ForEach(viewModel.users, id: \.id) { user in
+                            NavigationLink(destination: UserDetailScreen(user: user) ) {
+                                UserListRowView(user: user)
+                            }
                         }
                     }
+                    .searchable(text: $viewModel.searchText, prompt: Text(AppConstants.placeholderSearchUsers))
                 }
-                .searchable(text: $viewModel.searchText, prompt: Text(AppConstants.placeholderSearchUsers))
             }
         case .idle:
             EmptyView()
